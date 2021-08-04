5b15857ede730e58cb40f85c804bed733250534c
sjsilva <sjonsilvap@gmail.com>
2021-08-04 16:40:35 -0300

    Implementing grouping search

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Linq; 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -10,10 +10,8 @@ namespace SalesWebMvc.Models
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
-
         public Department()
         {
-
         }
 
         public Department(int id, string name)
@@ -27,11 +25,9 @@ namespace SalesWebMvc.Models
             Sellers.Add(seller);
         }
 
-        public double TotalSales(DateTime initial,  DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
-
-
     }
 }
